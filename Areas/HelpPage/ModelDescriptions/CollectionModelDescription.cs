6fe0aed18a0e0136210a98ca66e1b0569916e96a
matthewmorris41 <matthewmorris41@gmail.com>
2015-08-03 20:51:05 -0400

    Creating a project
    these are the things you go though when you create a project

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BookService2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookService2.WebApiApplication" Language="C#" %>
